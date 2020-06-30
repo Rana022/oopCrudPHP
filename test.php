2e55091bb7030fbded31a257f0a844474352f006
Ghosh022 <ghosh1706022@gmail.com>
2020-06-30 16:32:06 +0600

    first

---
+++ test.php
@@ -0,0 +1,11 @@
+<?php
+include "index.php";
+$obj = new query;
+$condition_arr = array("name" => "bisu", "mobile" => "333333");
+//print_r($condition_arr);
+//$result = $obj->getData("*", "emp", $condition_arr, "id", "desc", "45");
+//$result = $obj->insertData("users", $condition_arr);
+//$result = $obj->deleteData("users", $condition_arr);
+//$result = $obj->updateData("users", $condition_arr, "id", 3);
+//echo "<pre>";
+//print_r($result);
